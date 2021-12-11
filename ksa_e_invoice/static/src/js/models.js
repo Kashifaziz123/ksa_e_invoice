ffa2f35f992c5850de42c419695d268098008179
Kashif Aziz <kashif.aziz@uhd.edu.iq>
2021-12-11 14:18:54 +0500

    Signed-off-by: Kashif Aziz <kashif.aziz@uhd.edu.iq>

--- ksa_e_invoice/static/src/js/models.js
+++ ksa_e_invoice/static/src/js/models.js
@@ -5,6 +5,5 @@ var models = require('point_of_sale.models');
 
 models.load_fields('res.company', ['street','street2','city','state_id','vat']);
 models.load_fields('product.product',['name']);
-
 })
 
